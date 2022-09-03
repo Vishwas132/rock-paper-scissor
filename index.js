99a11cb6fc5cf825b8d05490f376f9e31a00db99
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-09-03 23:53:02 +0530

    Implement simple UI without CSS

--- index.js
+++ index.js
@@ -1,44 +1,74 @@
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissor"];
-    const  random = Math.floor((Math.random() * 3));
-    return choices[random];
+  const choices = ["rock", "paper", "scissor"];
+  const random = Math.floor(Math.random() * 3);
+  return choices[random];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
-        return 0;
-    } else if(playerSelection === "rock") {
-        return computerSelection === "scissor" ? "win" : "lose";
-    } else if(playerSelection === "paper") {
-        return computerSelection === "rock" ? "win" : "lose";
-    } else if(playerSelection === "scissor") {
-        return computerSelection === "paper" ? "win" : "lose";
-    }
+function displayText(text, id) {
+  const div = query(id);
+  div.textContent = text;
 }
 
 function playGame() {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt();
-        const computerSelection = getComputerChoice();
-        console.log("Round " + (i+1));
-        console.log("Your choice: " + playerSelection);
-        console.log("Computer choice: " + computerSelection);
-        const roundResult = playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
-        if(roundResult === "win") {
-            scorePlayer++;
-        } else if(roundResult === "lose") {
-            scoreComputer++;
-        }
-    }
-    if(scorePlayer > scoreComputer) {
-        console.log("You win!");
-    } else if(scorePlayer < scoreComputer) {
-        console.log("You Lose!");
-    } else {
-        console.log("It's a draw!");
-    }
+  if (query("#new-game")) {
+    query("#new-game").remove();
+  }
+  rockButton.addEventListener("click", playRound);
+  paperButton.addEventListener("click", playRound);
+  scissorButton.addEventListener("click", playRound);
+  scorePlayer = 0;
+  scoreComputer = 0;
+  displayText("", "#result");
+  displayText("0", "#player-score");
+  displayText("0", "#computer-score");
+  displayText("", "#player-choice");
+  displayText("", "#computer-choice");
 }
 
+function playRound(event) {
+  const playerSelection = event.currentTarget.id;
+  const computerSelection = getComputerChoice();
+  displayText(playerSelection, "#player-choice");
+  displayText(computerSelection, "#computer-choice");
+
+  if (playerSelection === computerSelection) {
+    return;
+  } else if (playerSelection === "rock") {
+    computerSelection === "scissor" ? scorePlayer++ : scoreComputer++;
+  } else if (playerSelection === "paper") {
+    computerSelection === "rock" ? scorePlayer++ : scoreComputer++;
+  } else if (playerSelection === "scissor") {
+    computerSelection === "paper" ? scorePlayer++ : scoreComputer++;
+  }
+  displayText(scorePlayer.toString(), "#player-score");
+  displayText(scoreComputer.toString(), "#computer-score");
+
+  if (scorePlayer === 5) {
+    displayText("you Win!", "#result");
+    restartGame();
+  } else if (scoreComputer === 5) {
+    displayText("You lose!", "#result");
+    restartGame();
+  }
+}
+
+function restartGame() {
+  rockButton.removeEventListener("click", playRound);
+  paperButton.removeEventListener("click", playRound);
+  scissorButton.removeEventListener("click", playRound);
+  const newGame = document.createElement("button");
+  newGame.setAttribute("id", "new-game");
+  newGame.append("New Game");
+  query("body").append(newGame);
+  newGame.addEventListener("click", playGame);
+}
+
+let scorePlayer = 0;
+let scoreComputer = 0;
+const query = function (selector) {
+  return document.querySelector(selector);
+};
+const rockButton = query("#rock");
+const paperButton = query("#paper");
+const scissorButton = query("#scissor");
 playGame();
